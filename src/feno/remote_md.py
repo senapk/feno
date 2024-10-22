2890858ce669a5153385c5224f957a6846ae0b86
lion <sena.ufc@gmail.com>
2024-10-22 17:08:41 -0300

    fix get label from folder

--- src/feno/remote_md.py
+++ src/feno/remote_md.py
@@ -8,12 +8,15 @@ from typing import List, Optional
 class Title:
     @staticmethod
     def extract_title(readme_file):
+        
+        folder = os.path.basename(os.path.dirname(readme_file))
+
         title = open(readme_file).read().split("\n")[0]
         parts = title.split(" ")
         if parts[0].count("#") == len(parts[0]):
             del parts[0]
         title = " ".join(parts)
-        return title
+        return "@" + folder + ": " + title
 
 class RemoteLink:
     def __init__(self):
