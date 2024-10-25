ec1fae48cdc3e2388be8d2035e0d6565130d9626
David Sena <sena.ufc@gmail.com>
2024-10-25 13:34:20 -0300

    fix encoding

--- src/feno/decoder.py
+++ src/feno/decoder.py
@@ -1,4 +1,5 @@
 import chardet
+import os
 
 class Decoder:
 
@@ -6,10 +7,15 @@ class Decoder:
     def load(file_path: str) -> str:
         with open(file_path, "rb") as file:
             raw_data = file.read()
-            encoding = chardet.detect(raw_data)["encoding"]
+            enc_dict = chardet.detect(raw_data)
+            encoding = enc_dict["encoding"]
             if encoding is None:
                 encoding = "utf-8"
-            return raw_data.decode(encoding)
+            try:
+                content = raw_data.decode(encoding)
+            except UnicodeDecodeError as e:
+                content = raw_data.decode("utf-8")
+            return content
 
     @staticmethod
     def save(file_path: str, content: str):
